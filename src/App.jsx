2650371ae2120aa27513620d9eaa17d4a1b58e09
Wilson Siaw <wilsonsiawse@gmail.com>
2024-03-13 22:12:33 +0200

    ✨ feat(Webpage): Import component
    Import and use the Skills component in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Hero from './components/Hero/Hero'
+import Skills from './components/Skills/Skills'
 
 function App() {
   
   return (
     <>
       <Hero />
+      <Skills />
     </>
   )
 }

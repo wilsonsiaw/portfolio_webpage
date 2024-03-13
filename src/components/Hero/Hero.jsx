0bce6fbe7c80cff94ea9a01661aa92a82ee2a6ec
Wilson Siaw <wilsonsiawse@gmail.com>
2024-03-13 21:06:13 +0200

    ✨ feat(Hero component): Add selectors
    Create selectors to help in positioning elements using CSS. Also remove the image tag. The profile image will be rendered using CSS

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Hero.css"
-import profile from "../../../public/assets/profile_pic3.jpg";
+import profile from "/assets/profile_pic3.jpg";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
@@ -8,29 +8,33 @@ import { BsTwitterX } from "react-icons/bs";
 const Hero = () => {
   return (
     <div className='hero-container'>
-      <h1>wilsonsiaw</h1>
-      <div className="icons">
-        <FaGithub />
-        <FaLinkedin />
-        <BsTwitterX />
+      <div className="hero-top">
+        <h1>wilsonsiaw</h1>
+        <div className="icons">
+          <FaGithub />
+          <FaLinkedin />
+          <BsTwitterX />
+        </div>
       </div>
-      <div className="profile">
-        <img src={profile} alt="profile picture" />
-      </div>
-      <div className='hero-content'>
-        <h2>
-            I'm <span id='name'>Wilson Siaw</span>
-            <br />
-            It's a pleasure to meet you!
-        </h2>
-        <p>
-            Based in South Africa, I'm a front-end developer
-            who delights in building interactive and user-friendly web apps.
-        </p>
-      </div>
-      <div className="contact-me">
-        <h3>CONTACT ME</h3>
-        <hr id='contact-line'/>
+      <div className="hero-center">
+        <div className="profile"></div>
+        <div className="hero-bottom">
+          <div className='hero-content'>
+            <h2>
+                I'm <span id='name'>Wilson Siaw.</span>
+                <br />
+                It's a pleasure to meet you!
+            </h2>
+            <p>
+                Based in South Africa, I'm a front-end developer
+                who delights in building interactive and user-friendly web apps.
+            </p>
+          </div>
+          <div className="contact-me">
+            <h3>CONTACT ME</h3>
+            <hr id='contact-line'/>
+          </div>
+        </div>
       </div>
     </div>
   )

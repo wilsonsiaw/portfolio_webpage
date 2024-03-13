efed10823d859027049af4878fb460040dd4b2b2
Wilson Siaw <wilsonsiawse@gmail.com>
2024-03-13 22:13:28 +0200

    ✨ feat(Skills component): Add code
    Write the code for the skills section

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className='skills-container'>
+        <hr id='skills-line-1'/>
+        <div className="skills">
+            <h3>HTML</h3>
+            <h3>CSS</h3>
+            <h3>Javascript</h3>
+            <h3>React</h3>
+            <h3>Responsiveness</h3>
+        </div>
+        <hr id='skills-line-2'/>
+    </div>
+  )
+}
+
+export default Skills

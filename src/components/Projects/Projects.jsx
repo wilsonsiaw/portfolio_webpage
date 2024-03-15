282c47006cfc08419b2cd229e12d8ef892f275ef
Wilson Siaw <wilsonsiawse@gmail.com>
2024-03-15 16:48:49 +0200

    🦄 refactor(Projects component): Add selectors
    Create new divs and selectors to make it easier to style the component using CSS

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
+import "./Projects.css"
 import data from "../../data.json"
 
 const Projects = () => {
 
     const projectElements = data.map(project => (
-        <div key={project.id} className='project-container'>
+        <div key={project.id} className='projects'>
             <div className='project-image'>
                 <img src={project.image} alt="" width={345}/>
             </div>
             <h3 className='project-title'>{project.name}</h3>
-            <h4 className='technology'>{project.technology[0]} {project.technology[1]}</h4>
+            <div className='technology'>
+                <h4>{project.technology[0].toUpperCase()}</h4> 
+                <h4>{project.technology[1].toUpperCase()}</h4>
+            </div>
             <div className='project-links'>
-                <a href="">VIEW PROJECT</a>
-                <a href="">VIEW CODE</a>
+                <a href={project.deployment_url} target='_blank' rel="noopener noreferrer">VIEW PROJECT</a>
+                <a href={project.github_url} target='_blank' rel='noopener noreferrer'>VIEW CODE</a>
             </div>
         </div>
     ))
 
     return (
-        <div>
-            <h3>Projects</h3>
-            <h3>CONTACT ME</h3>
-            {projectElements}
+        <div className='container'>
+            <div className="container-top">
+                <h3>Projects</h3>
+                <h3>CONTACT ME</h3>
+            </div>
+            <hr id='project-line'/>
+            <div className="project-container">
+                {projectElements}
+            </div>
         </div>
     )
 }

97e0f8aa92513ec12d024a7e1ccc4556a972f148
Wilson Siaw <wilsonsiawse@gmail.com>
2024-03-15 12:46:41 +0200

    🐞 fix(Hero component): Remove import
    Remove the import statement for the profile picture

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./Hero.css"
-import profile from "/assets/profile_pic3.jpg";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";

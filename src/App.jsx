6254b287782ee7ab48eb45b819da9f5a4ce5abb5
Wilson Siaw <wilsonsiawse@gmail.com>
2024-03-18 21:23:17 +0200

    ✨ feat(App component): Import component
    Import and use the Footer component to be rendered by the App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </>
   )
 }

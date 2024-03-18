1d67982443e3b53985cb055596e1bbfd555fa7b6
Wilson Siaw <wilsonsiawse@gmail.com>
2024-03-18 21:25:04 +0200

    ✨ feat(Footer component ): Write code
    Write the code for the component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./Footer.css"
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+
+const Footer = () => {
+  return (
+    <div className='footer-name'>
+        <hr id='footer-line'/>
+        <div className="footer-content">
+            <h4>wilsonsiaw</h4>
+            <div className='footer-icons'>
+                <a 
+                    href="https://github.com/wilsonsiaw"
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    <FaGithub />
+                </a>
+                <a 
+                    href="https://www.linkedin.com/in/wilson-siaw/"
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    <FaLinkedin />
+                </a>
+                <a 
+                    href="https://twitter.com/data_wilson"
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    <BsTwitterX />
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer

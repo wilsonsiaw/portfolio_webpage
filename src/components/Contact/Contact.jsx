903021beffe34ec5e4ab4e5c177cc072a46f0fb2
Wilson Siaw <wilsonsiawse@gmail.com>
2024-03-18 19:58:06 +0200

    🐞 fix(Contact component): Remove line breaks
    Remove line breaks. The line breaks are handled in the CSS

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,36 +4,38 @@ import "./Contact.css"
 const Contact = () => {
   return (
     <div className='contact-container'>
-        <div className="contact-top">
-            <h4>Contact</h4>
-            <p>
-                I would love to hear about your project and any software development
-                opportunities you have.
-                <br />
-                I'm looking forward to discussing how I can help.
-                Please fill in the form, and I'll get back to you as soon as
-                possible.
-            </p>
-        </div>
+        <div className="contact">
+            <div className="contact-top">
+                <h4>Contact</h4>
+                <p>
+                    I would love to hear about your project and any software development
+                    opportunities you have.
+                    <br />
+                    I'm looking forward to discussing how I can help.
+                    Please fill in the form, and I'll get back to you as soon as
+                    possible.
+                </p>
+            </div>
 
-        <form action="">
-            <input type="text" 
-                placeholder='NAME'
-            />
-            <br />
-            <input type="email" 
-                placeholder='EMAIL'
-            />
-            <br />
-            <textarea 
-                name="" 
-                id="" cols="30" 
-                rows="10" 
-                placeholder='MESSAGE'>
-            </textarea>
-            <br />
-            <button>SEND MESSAGE</button>
-        </form>
+            <form action="" className='contact-form'>
+                <input type="text"
+                    placeholder='NAME'
+                />
+                <input type="email"
+                    placeholder='EMAIL'
+                />
+                <textarea
+                    name=""
+                    id="" cols="30"
+                    rows="10"
+                    placeholder='MESSAGE'>
+                </textarea>
+                <div className="submit-btn">
+                    <button>SEND MESSAGE</button>
+                </div>
+                <hr id='submit-line'/>
+            </form>
+        </div>
     </div>
   )
 }
